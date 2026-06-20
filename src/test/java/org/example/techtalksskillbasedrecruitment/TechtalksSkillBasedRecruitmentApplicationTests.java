b0f724ade4974629ebc48b535c577cc2d888bde2
odayaridi <odayaridi.se@gmail.com>
2026-06-20 16:55:39 +0300

    Initial commit

---
+++ src/test/java/org/example/techtalksskillbasedrecruitment/TechtalksSkillBasedRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.techtalksskillbasedrecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtalksSkillBasedRecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
