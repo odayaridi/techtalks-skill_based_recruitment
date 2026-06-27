15ea256585a4d0c8cb780617a819b1f987ced790
majd-alhalabi <majd.halabi9@gmail.com>
2026-06-28 00:39:58 +0300

    implement skill management CRU APIs

--- src/main/java/org/example/techtalksskillbasedrecruitment/skill/SkillController.java
+++ src/main/java/org/example/techtalksskillbasedrecruitment/skill/SkillController.java
@@ -1,4 +1,39 @@
 package org.example.techtalksskillbasedrecruitment.skill;
 
+
+import jakarta.validation.Valid;
+import org.example.techtalksskillbasedrecruitment.skill.dto.request.SkillRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/skills")
 public class SkillController {
+    private final SkillService skillService;
+
+    public SkillController(SkillService skillService) {
+        this.skillService = skillService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Skill> createSkill(@Valid @RequestBody SkillRequest request) {
+        return ResponseEntity.ok(skillService.createSkill(request));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Skill>> getAllSkills() {
+        return ResponseEntity.ok(skillService.getAllSkills());
+    }
+
+    @GetMapping("/get/skillId/{skillId}")
+    public ResponseEntity<Skill> getSkillById(@PathVariable Integer skillId) {
+        return ResponseEntity.ok(skillService.getSkillById(skillId));
+    }
+
+    @PutMapping("/update/skillId/{skillId}")
+    public ResponseEntity<Skill> updateSkill(@PathVariable Integer skillId, @Valid @RequestBody SkillRequest request) {
+        return ResponseEntity.ok(skillService.updateSkill(skillId, request));
+    }
 }
--- src/main/java/org/example/techtalksskillbasedrecruitment/skill/SkillService.java
+++ src/main/java/org/example/techtalksskillbasedrecruitment/skill/SkillService.java
@@ -1,4 +1,37 @@
 package org.example.techtalksskillbasedrecruitment.skill;
 
+
+import org.example.techtalksskillbasedrecruitment.exceptions.ResourceNotFoundException;
+import org.example.techtalksskillbasedrecruitment.skill.dto.request.SkillRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SkillService {
+    private final SkillRepository skillRepository;
+
+    public SkillService(SkillRepository skillRepository) {
+        this.skillRepository = skillRepository;
+    }
+
+    public Skill createSkill(SkillRequest request) {
+        Skill skill = new Skill();
+        skill.setSkillName(request.getSkillName());
+        return skillRepository.save(skill);
+    }
+
+    public List<Skill> getAllSkills() {
+        return skillRepository.findAll();
+    }
+
+    public Skill getSkillById(Integer skillId) {
+        return skillRepository.findById(skillId).orElseThrow(() -> new ResourceNotFoundException("Skill not found with id: " + skillId));
+    }
+
+    public Skill updateSkill(Integer skillId, SkillRequest request) {
+        Skill skill = skillRepository.findById(skillId).orElseThrow(() -> new ResourceNotFoundException("Skill not found with id: " + skillId));
+        skill.setSkillName(request.getSkillName());
+        return skillRepository.save(skill);
+    }
 }
