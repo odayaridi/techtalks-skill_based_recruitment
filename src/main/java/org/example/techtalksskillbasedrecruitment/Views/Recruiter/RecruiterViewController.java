5bd6652239ae94a163f134857ce9a6c8ce7b66ca
Moustafa <m71443670@gmail.com>
2026-06-27 17:48:21 +0300

    Implement getRecruiterByUserId API

--- src/main/java/org/example/techtalksskillbasedrecruitment/Views/Recruiter/RecruiterViewController.java
+++ src/main/java/org/example/techtalksskillbasedrecruitment/Views/Recruiter/RecruiterViewController.java
@@ -1,4 +1,18 @@
 package org.example.techtalksskillbasedrecruitment.Views.Recruiter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/recruiters")
+
 public class RecruiterViewController {
+
+    @Autowired
+    private RecruiterViewService RecServ;
+
+    @GetMapping("/{id}")
+    public RecruiterView getRecruiterByUserId(@PathVariable Integer id){
+        return RecServ.getRecruiterByUserId(id);
+    }
 }
--- src/main/java/org/example/techtalksskillbasedrecruitment/Views/Recruiter/RecruiterViewRepository.java
+++ src/main/java/org/example/techtalksskillbasedrecruitment/Views/Recruiter/RecruiterViewRepository.java
@@ -1,6 +1,7 @@
 package org.example.techtalksskillbasedrecruitment.Views.Recruiter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public interface RecruiterViewRepository
         extends JpaRepository<RecruiterView, Integer> {
