64bd6b65de6ccbbaf84e1da7c739965db7f7c092
ysfbzl <youssif.bzl@gmail.com>
2026-06-28 12:14:09 +0300

    Implement certification profile and candidate view APIs

--- src/main/java/org/example/techtalksskillbasedrecruitment/Views/Candidate/CandidateViewController.java
+++ src/main/java/org/example/techtalksskillbasedrecruitment/Views/Candidate/CandidateViewController.java
@@ -1,4 +1,24 @@
 package org.example.techtalksskillbasedrecruitment.Views.Candidate;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", allowCredentials = "false")
+@RestController
+@RequestMapping("/api/candidate-view")
 public class CandidateViewController {
+
+    private final CandidateViewService candidateViewService;
+
+    public CandidateViewController(CandidateViewService candidateViewService) {
+        this.candidateViewService = candidateViewService;
+    }
+
+    @GetMapping("/getCandidateByUserId/{userId}")
+    public ResponseEntity<CandidateView> getCandidateByUserIdController(@PathVariable Integer userId) {
+        CandidateView candidateView = this.candidateViewService.getCandidateByUserIdService(userId);
+
+        return new ResponseEntity<>(candidateView, HttpStatus.OK);
+    }
 }
--- src/main/java/org/example/techtalksskillbasedrecruitment/candidateprofile/CandidateProfileRepository.java
+++ src/main/java/org/example/techtalksskillbasedrecruitment/candidateprofile/CandidateProfileRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CandidateProfileRepository extends JpaRepository<CandidateProfile,Integer> {
     boolean existsByUser(User user);
     CandidateProfile findByUser(User user);
+
 }
 
 
